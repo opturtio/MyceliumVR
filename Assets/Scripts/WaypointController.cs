4d43e2d620046acaf54538d105be7616e676edc0
Lauri Lyytikäinen <lauri.lyytikainen1@gmail.com>
2023-11-23 21:08:30 +0200

    added waypoints

--- Assets/Scripts/WaypointController.cs
+++ Assets/Scripts/WaypointController.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class WaypointController : MonoBehaviour
 {
     public int type;
     public WaypointManager manager;
+    public VisualEffect effect;
+
+
+    public void SetColor(Color color)
+    {
+        effect.SetVector4("Color", color);
+    }
 
     // Start is called before the first frame update
     void Start()
